43ba41341246309d0e3c268b5c59bbb6234e69dd
Lea Äyhynmäki <oleay001@edu.xamk.fi>
2018-04-11 14:14:29 +0300

    1142018.3.3

--- script/koodi.js
+++ script/koodi.js
@@ -37,4 +37,16 @@ $(document).ready(function(){
         $("#etusivu").addClass("piilo");
     });
     
+    function tulostatuotteet(result){
+        var ateriat;
+        
+        console.log(result.ruokalista[1].nimi);
+        
+        /*for(var i=0; i < result.length; i++){
+                ateriat += "<h1>" + result[2].nimi + "</h1><div class='tiedot piilo'>" + result[2].ainekset + result[2].hinta + "</div>";
+            }
+        
+        $("#ruokalista").html(ateriat);*/
+    }
+    
 });
