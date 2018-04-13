b5c2ffb089c1f9d7c86816a0780390f010b591b2
Lea Äyhynmäki <oleay001@edu.xamk.fi>
2018-04-13 23:26:06 +0300

    1342018.1.3.123

--- script/koodi_yllapito.js
+++ script/koodi_yllapito.js
@@ -7,17 +7,18 @@ $(document).ready(function(){
             });
     
     var ateriat="<div id='eka' class='col-lg-6'>";
+    var napit="<div class='oikea napit'><button>Muokkaa</button><button data-toggle='modal' data-target='#poista'>Poista</button></div>"
     
     //Tulostetaan haetut areriat sivulle
     function tulostatuotteet(result){
         
         for(i=0; i < result.ruokalista.length; i++){
-                ateriat +="<h1>" + result.ruokalista[i].nimi + "<span class='glyphicon glyphicon-remove ml10' data-toggle='modal' data-target='#myModal'></span><span class='glyphicon glyphicon-edit'></span></h1><div class='tiedot piilo'>" + result.ruokalista[i].ainekset + " <b>" + result.ruokalista[i].hinta  + "€</b></div>";
+                ateriat +="<h1>" + result.ruokalista[i].nimi + "<span class='glyphicon glyphicon-remove ml10' data-toggle='modal' data-target='#poista'></span><span class='glyphicon glyphicon-edit'></span></h1><div class='tiedot piilo'>" + result.ruokalista[i].ainekset + " <b>" + result.ruokalista[i].hinta  + "€</b>" + napit + "</div>";
             
                 if(i==13){
                     ateriat+="</div><div id='toka' class='col-lg-6'>";
                     for(i=13; i < result.ruokalista.length; i++){
-                        ateriat +="<h1>" + result.ruokalista[i].nimi + "<span class='glyphicon glyphicon-remove ml10' data-toggle='modal' data-target='#myModal'></span><span class='glyphicon glyphicon-edit'></span></h1><div class='tiedot piilo'>" + result.ruokalista[i].ainekset + " <b>" + result.ruokalista[i].hinta  + "€</b></div>";
+                        ateriat +="<h1>" + result.ruokalista[i].nimi + "<span class='glyphicon glyphicon-remove ml10' data-toggle='modal' data-target='#poista'></span><span class='glyphicon glyphicon-edit'></span></h1><div class='tiedot piilo'>" + result.ruokalista[i].ainekset + " <b>" + result.ruokalista[i].hinta  + "€</b>" + napit + "</div>";
                     }
                 }
             }
